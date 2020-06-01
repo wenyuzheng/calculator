8727c0c34e22a841160ac7764b9a0047b9b87a7a
Wenyu Zheng <zhengwy229@gmail.com>
2020-06-01 09:37:02 +0100

    plus button and color button

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
 import React from 'react';
-import logo from './logo.svg';
+import {useState} from 'react';
 import './App.css';
 
-function App() {
+const App = () => {
+  const array1 = useState(0)
+  const count = array1[0]
+  const setCount = array1[1]
+
+  const array2 = useState("green")
+  const color = array2[0]
+  const setColor = array2[1]
+  
+  const plusHandler = () => {
+    const newCount = count + 1
+    setCount(newCount)
+  }
+
+  const colorHandler = () => {
+    if (color === 'green') {
+      setColor("red")
+    } else {
+      setColor("green")
+    }
+  }
+
+  const style = { color: color}
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={style}>
+      <h1>{"clicked " + count + " times"}</h1>
+      <div onClick={plusHandler}>+++</div>
+      <div onClick={colorHandler}>color </div>
     </div>
   );
 }
