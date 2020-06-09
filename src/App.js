d5ac39bf4c27bd26884a02f456bebe878deb896b
Wenyu Zheng <zhengwy229@gmail.com>
2020-06-09 15:59:59 +0100

    a working calculator

--- src/App.js
+++ src/App.js
@@ -2,50 +2,6 @@ import React from 'react';
 import {useState} from 'react';
 import './App.css';
 import MyButton from './MyButton';
-import {HotKeys} from 'react-hotkeys';
-
-// const MyButton = ({number, handler}) => {
-//   return (
-//     <button onClick={() => handler(number)}>{number}</button>
-//   )
-// }
-
-// const App = () => {
-//   const [count, setCount] = useState("");
-
-//   const plusHandler = (number) => {
-//     const newCount = count + number;
-//     setCount(newCount);
-//   }
-
-// const numbers = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'];
-
-//   return (
-//     <div className="App">
-//       <h1>{count}</h1>
-
-//       {numbers.map((num)=> {
-//         return (
-//           <MyButton number={num} handler={plusHandler} key={num}/>
-//         )
-//       })}
-//     </div>
-//   );
-// }
-
-// export default App;
-
-
-
-
-// Work To Do: 
-// 1. Get all buttons to turn grey on click
-// 2. Make Ans button work
-// 3. Use display grid for css
-
-//Improvement: 
-// 1. Handlers can be used directly in onClick
-// 2. Make hotkeys (ie press on keybroad)
 
 const App = () => {
   
@@ -53,42 +9,57 @@ const App = () => {
 
   const [answer, setAnswer] = useState("");
 
-  const [storedAnswer, setStoredAnswer] = useState("");
+  const [displayAns, setDisplayAns] = useState(false);
 
   const onClickHandler = (number) => {
-    setStateValue(stateValue + number);
+    if (number === "=") {
+      equalHandler();
+    } else if (number === "C") {
+      clearHandler();
+    } else if (number === "Ans") {
+      answerHandler();
+    } else if (number === "Del") {
+      deleteHandler();
+    } else {
+      setStateValue(stateValue + number);
+    }
   }
 
   const deleteHandler = () => {
-    setStateValue(stateValue.slice(0, -1));
+    if (stateValue.slice(stateValue.length-3, stateValue.length) === "Ans") {
+      setStateValue(stateValue.slice(0, -3));
+    } else {
+      setStateValue(stateValue.slice(0, -1));
+    }
   }
 
-  const equalHandler = () => { // Warining: eval() is harmful!
+  const equalHandler = () => { 
     try {
-      setAnswer(eval(stateValue)); 
+      const newAnswer = stateValue.replace("Ans", answer);
+      setDisplayAns(true);
+      setAnswer(eval(newAnswer)); 
     } catch {
       alert("Invalid equation!!!");
     }
   }
 
-  const answerHandler = () => {
-    setStoredAnswer(answer);
-    console.log(storedAnswer);
-  }
-
   const clearHandler = () => {
-    setStateValue("");
-    setAnswer("");
+    setStateValue(""); 
+    setDisplayAns(false);
   }
 
-  const numbers = ["1", "2", "3", "(", ")", "4", "5", "6", "+", "-","7", "8", "9", "*", "/", ".", "0", "Ans", "=", "C", "Del" ]
+  const answerHandler = () => {
+    setStateValue(stateValue + "Ans");
+  }
+
+  const numbers = ["1", "2", "3", "(", ")", "4", "5", "6", "+", "-","7", "8", "9", "*", "/", ".", "0", "Ans", "=", "C", "Del"]
 
   return (
     <div className="out">
 
       <div className="InOutput">
         <div className="UserInput">{stateValue}</div>
-        <div className="CalculatorOutput">{answer}</div>
+        <div className="CalculatorOutput">{displayAns ? answer : ""}</div>
       </div>
 
       <div>
@@ -103,11 +74,6 @@ const App = () => {
                 <MyButton {...myButtonProps}/>
               )
             })}
-
-            {/* <button onClick={deleteHandler}>Del</button>
-            <button onClick={answerHandler}>Ans</button>
-            <button onClick={equalHandler}>=</button> */}
-            <button className = "item" onClick={clearHandler}>C</button>
         </div>
       </div>
       
@@ -116,3 +82,6 @@ const App = () => {
 }
 
 export default App;
+
+
+// Question: How to apply className="item" to button C?
